7f799dd0baa114f6a4abcc7574e13ef53e5652c3
felipeagpx <69019342+felipeagpx@users.noreply.github.com>
2020-09-03 22:53:17 -0300

    Add files via upload

---
+++ AtividadeAula05.php
@@ -0,0 +1,23 @@
+<?php
+
+$num1 = $_GET ['num1'];
+$num2 = $_GET ['num2'];
+
+if($num2 == 0)
+{
+    echo('Não é possível dividir por zero.'.'<br>');
+}
+else
+{
+    $divisao = $num1/$num2;
+    echo ("O valor da divisão é: $divisao".'<br>');
+}
+
+$multi = $num1*$num2;
+echo ("O valor da multiplicação é: $multi".'<br>');
+
+$soma = $num1+$num2;
+echo ("O valor da soma é: $soma".'<br>');
+
+$sub = $num1-$num2;
+echo ("O valor da subtração é: $sub".'<br>');
